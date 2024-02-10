6830e9dff39081f9cf497e1abdf7d33969633a51
dogerish <tetsayeah22@gmail.com>
2024-02-10 12:23:58 -0500

    Basic set-up from template

---
+++ src/components/CrosswordPuzzle.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Crossword Puzzle</h1>
+</template>
+
+<script>
+export default {
+  name: "CrosswordPuzzle",
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <CrosswordPuzzle />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import CrosswordPuzzle from "@/components/CrosswordPuzzle.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    CrosswordPuzzle,
   },
 };
 </script>
